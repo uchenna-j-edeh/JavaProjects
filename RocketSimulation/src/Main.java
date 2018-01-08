bd4872a9ca16bb0330835d3b9029cc0b50ba351f
uchenna-j-edeh <write2uche@gmail.com>
2018-01-07 19:30:42 -0600

    clean up...

--- RocketSimulation/src/Main.java
+++ RocketSimulation/src/Main.java
@@ -6,29 +6,22 @@ public class Main {
         ArrayList<Rocket> u1Rockets = new ArrayList();
         U1 rock1 = new U1();
 
-        ArrayList<Item> phase1StuffOnBoard1 = new ArrayList<Item>();
-        ArrayList<Item> phase2StuffOnBoard1 = new ArrayList<Item>();
-
-        ArrayList<U1> ph1Rockets1 = new ArrayList<U1>();
-        ArrayList<U1> ph2Rockets1 = new ArrayList<U1>();
-
         Simulation sim = new Simulation();
-        phase1StuffOnBoard1 = sim.loadItems("Phase1ListOfItems.txt");
-        phase2StuffOnBoard1 = sim.loadItems("Phase2ListOfItems.txt");
+        ArrayList<Item> phase1StuffOnBoard1 = sim.loadItems("Phase1ListOfItems.txt");
+        ArrayList<Item> phase2StuffOnBoard1 = sim.loadItems("Phase2ListOfItems.txt");
 
-        ph1Rockets1 = sim.loadU1(phase1StuffOnBoard1);
+        ArrayList<U1> ph1Rockets1 = sim.loadU1(phase1StuffOnBoard1);
         for (Rocket r1 : ph1Rockets1) {
             u1Rockets.add(r1);
         }
-        ph2Rockets1 = sim.loadU1(phase2StuffOnBoard1);
+
+        ArrayList<U1> ph2Rockets1 = sim.loadU1(phase2StuffOnBoard1);
         for (Rocket r1 : ph2Rockets1) {
             u1Rockets.add(r1);
         }
 
         int totalPhlRock1 = sim.runSimulation(u1Rockets);
-
         long value = totalPhlRock1 * rock1.budget;
-
         return value;
     }
 
@@ -36,30 +29,23 @@ public class Main {
         ArrayList<Rocket>  u2Rockets = new ArrayList();
         U2 rock2 = new U2();
 
-        ArrayList <Item> phase1StuffOnBoard2 = new ArrayList<Item>();
-        ArrayList <Item> phase2StuffOnBoard2 = new ArrayList<Item>();
-
-        ArrayList<U1> ph1Rockets2 = new ArrayList<U1>();
-        ArrayList<U1> ph2Rockets2 = new ArrayList<U1>();
-
         Simulation sim2 = new Simulation();
-        phase1StuffOnBoard2 = sim2.loadItems("Phase1ListOfItems.txt");
-        phase2StuffOnBoard2 = sim2.loadItems("Phase2ListOfItems.txt");
+        ArrayList <Item> phase1StuffOnBoard2 = sim2.loadItems("Phase1ListOfItems.txt");
+        ArrayList <Item> phase2StuffOnBoard2 = sim2.loadItems("Phase2ListOfItems.txt");
 
-        ph1Rockets2 = sim2.loadU1(phase1StuffOnBoard2);
+        ArrayList<U2> ph1Rockets2  = sim2.loadU2(phase1StuffOnBoard2);
         for(Rocket r1: ph1Rockets2){
             u2Rockets.add(r1);
         }
 
-        ph2Rockets2 = sim2.loadU1(phase2StuffOnBoard2);
+        ArrayList<U2> ph2Rockets2 = sim2.loadU2(phase2StuffOnBoard2);
         for(Rocket r2: ph2Rockets2){
             u2Rockets.add(r2);
         }
 
-        int totalPh2Rock1 = sim2.runSimulation(u2Rockets);
-        long value2 = totalPh2Rock1 * rock2.budget ;
-
-        return value2;
+        int total = sim2.runSimulation(u2Rockets);
+        long value = total * rock2.budget ;
+        return value;
     }
 
     public static void main(String [] args) throws Exception{
@@ -70,8 +56,7 @@ public class Main {
 
         //Calculate cost of launching rocket 2
         long cost2 = costOfRocket2();
-        System.out.println("U1 -> Total cost spent is: $" + cost2);
-
+        System.out.println("U2 -> Total cost spent is: $" + cost2);
 
     }
 }
